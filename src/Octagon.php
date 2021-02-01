fc011381c472d2633d79a808fb0697be12b3f0e7
Robert Evans <rmevans1@coastal.edu>
2021-01-31 23:30:30 -0500

    Completed Octagon Class

--- src/Octagon.php
+++ src/Octagon.php
@@ -3,5 +3,21 @@
 
 class Octagon extends Shape
 {
+    const SHAPE_TYPE = 4;
+    protected $side;
 
+    public function __construct($side)
+    {
+        parent::__construct(0, 0);
+        $this->side = $side;
+    }
+
+    public function area()
+    {
+        return 2 * (1 + sqrt(2)) * pow($this->side, 2);
+    }
+
+    public function getFullDescription(){
+        return "Octagon<#{$this->getId()}>: {$this->name} - {$this->side}";
+    }
 }
