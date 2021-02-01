b3c0100a22da31b163aa2472ee0122958cfb2446
Robert Evans <rmevans1@coastal.edu>
2021-01-31 23:30:05 -0500

    Completed Circle class

--- src/Circle.php
+++ src/Circle.php
@@ -3,12 +3,20 @@
 
 class Circle extends Shape
 {
-    const SHAPE_ID = 3;
+    const SHAPE_TYPE = 3;
     protected $radius;
 
     public function __construct($radius)
     {
         parent::__construct(0, 0);
         $this->radius = $radius;
+    }
+
+    public function area(){
+        return 3.14 * pow($this->radius, 2);
+    }
+
+    public function getFullDescription(){
+        return "Circle<#{$this->getId()}>: {$this->name} - {$this->radius}";
     }
 }
