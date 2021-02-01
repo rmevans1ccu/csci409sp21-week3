ff1e84e0861ee42dc2fbd53bec490b0aeef5f0c5
Robert Evans <rmevans1@coastal.edu>
2021-01-31 23:05:56 -0500

    Fixed Rectangle class

--- src/Rectangle.php
+++ src/Rectangle.php
@@ -4,4 +4,8 @@
 class Rectangle extends Shape
 {
     const SHAPE_TYPE = 2;
+
+    public function getFullDescription(){
+        return "Rectangle<#{$this->getId()}>: {$this->name} - {$this->length} x {$this->width}";
+    }
 }
